8f3737242cf8a08ebed7423b5baedfe405250bc8
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-01-15 12:47:25 -0300

    Page new Call

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -4,12 +4,77 @@ import Title from '../../components/Title';
 import './new.css';
 
 import { FiPlusCircle } from 'react-icons/fi';
+import { useState , useEffect , useContext } from 'react';
+import { AuthContext } from '../../Contexts/auth';
+import { async } from '@firebase/util';
+
+import { app } from '../../services/firebaseConection';
+import { getAuth } from 'firebase/auth';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 export default () => {
 
+    const [loading , setLoading] = useState(true);
+    const [customers , setCustomers] = useState([]);
+
+    const [customerSelected , setCustomerSelected] = useState(0);
+    const [description , setDescription] = useState('');
+    const [status , setStatus] = useState('Opened');
+    const [problem , setProblem] = useState("");
+
+    const database = getFirestore(app);
+
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        async function loadCustomers() {
+            let customersCollection = collection(database, "costumers");
+            let customersData = await getDocs(customersCollection)
+            .then((snapshot) => {
+                let arrayCustomers = [];
+
+                snapshot.forEach((doc) => {
+                    arrayCustomers.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                    })
+                });
+
+                if (arrayCustomers.length === 0) {
+                    toast.info("No one Client Founded!");
+                    setCustomers([]);
+                    setLoading(false);
+                    return;
+                }
+
+                setCustomers(arrayCustomers);
+                setLoading(false);
+            })
+            .catch((error) => {
+                toast.error("Fail Showing Customers!")
+                setLoading(false);
+                setCustomers([{id:1 , name: ''}]);
+            })
+        }
+
+        loadCustomers();
+    }, []);
+
     function newCall(e) {
         e.preventDefault();
-        alert("new call")
+    }
+
+    function changeSelect(e) {
+        setDescription(e.target.value);
+    }
+
+    function changeStatus(e) {
+        setStatus(e.target.value);
+    }
+
+    function changeSelecteCustomer(e) {
+        setCustomerSelected(e.target.value);
     }
 
     return(
@@ -24,12 +89,25 @@ export default () => {
                 <div className="container">
                     <form className='form-profile' onSubmit={newCall}>
                         <label htmlFor='select'>Costumer</label> <br /> 
-                        <select id='select' required>
-                            <option key={1} value={1}>Gabriel</option>
-                        </select> <br />
+
+                        {loading ? (
+                            <input type="text" disabled={true} value="Loading Customers..." />
+                         ) : (
+                            <select id='select' required value={customerSelected} onChange={changeSelecteCustomer}>
+                                {customers.map((item,index) => {
+                                    return(
+                                        <option key={item.id} value={index}>
+                                            {item.name}
+                                        </option>
+                                    )
+                                    })}
+                            </select>
+                        )}
+
+                        <br />
 
                         <label htmlFor='description'>Description</label> <br />
-                        <select id='description'>
+                        <select id='description' value={description} onChange={changeSelect}>
                             <option value="Suport" required>Suport</option>
                             <option value="financial">Financial</option>
                             <option value="technicalVisit">Technical Visit</option>
@@ -38,18 +116,18 @@ export default () => {
 
                         <label htmlFor='status'>status</label>
                         <div className='status'>
-                            <input type="radio" name="radio" value="Opened" id='opened' required/>
+                            <input type="radio" name="radio" value="Opened" id='opened' required onChange={changeStatus} checked={ status === 'Opened' }/>
                             <label htmlFor='opened'>Opened</label>
 
-                            <input type="radio" name="radio" value="Pending" id='pending'/>
+                            <input type="radio" name="radio" value="Pending" id='pending' onChange={changeStatus} checked={ status === 'Pending' }/>
                             <label htmlFor='pending'>Pending</label>
 
-                            <input type="radio" name="radio" value="Conclued" id='conclued'/>
+                            <input type="radio" name="radio" value="Conclued" id='conclued' onChange={changeStatus} checked={ status === 'Conclued' }/>
                             <label htmlFor='conclued'>Conclued</label>
                         </div>
 
                         <label htmlFor='problem'>Your Problem</label><br />
-                        <textarea type="text" placeholder='Your Problem' required /><br/>
+                        <textarea type="text" placeholder='Your Problem' required value={problem} onChange={(e) => setProblem(e.target.value)} /><br/>
 
                         <button type='submit'>Save</button>
                     </form>
